dfb095078f57e5252b6fd92c37a09892a6052a5a
Talhaasan <44949936+Talhaasan@users.noreply.github.com>
2021-02-12 13:06:59 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/question3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/question3.dir/main.cpp.obj"
+  "libquestion3.dll.a"
+  "question3.exe"
+  "question3.exe.manifest"
+  "question3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/question3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
